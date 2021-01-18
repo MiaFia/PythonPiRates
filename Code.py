bbe8591ef6d4bd1a020806fb042b04e2c084e2e9
Johanna <johanna.annau@gmail.com>
2021-01-18 11:58:29 +0100

    I added what I think we have to do in the shell

--- Code.py
+++ Code.py
@@ -2,9 +2,12 @@
 
 import json
 
-with open ('') as file:
-    real_people=load.json()
+#with open ('') as file:
+ #   real_people=load.json()
 
-    #use SelectString in CLI to only use data that you need 
 
-Select-String label 'person'
+#use SelectString in CLI to only use data that you need
+# So these are probably things to do in the shell
+#Get-Content 'people.zip'
+#Select-String label 'person'
+# Write-Output > 'new file with a name, that only includes non-fictional people
